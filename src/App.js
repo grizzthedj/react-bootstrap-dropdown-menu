f77bbcb115b9c9ffc83264612e64c4b6c96707ff
Chris Smith <chris.smith@q9.com>
2017-02-04 16:08:44 -0500

    Updating build configs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,2 @@
-// import DropdownMenu from './DropdownMenu';
-// import MenuItem from './MenuItem';
-//
-// export {
-//   DropdownMenu,
-//   MenuItem
-// };
-//
-// module.exports = {
-//   DropdownMenu: require('./DropdownMenu'),
-//   MenuItem: require('./MenuItem')
-// }
 
 module.exports = require('./DropdownMenu');
