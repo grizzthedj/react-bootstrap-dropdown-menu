119dcdf49a5ea39f5a5071f248dda94ce529b1a0
Chris Smith <chris.smith@q9.com>
2017-02-04 10:41:36 -0500

    Trying to fix the build

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import DropdownMenu from './DropdownMenu';
-import MenuItem from './MenuItem';
-
-export {
-  DropdownMenu,
-  MenuItem
-};
-
-
-
+// import DropdownMenu from './DropdownMenu';
+// import MenuItem from './MenuItem';
+//
+// export {
+//   DropdownMenu,
+//   MenuItem
+// };
+//
 // module.exports = {
 //   DropdownMenu: require('./DropdownMenu'),
 //   MenuItem: require('./MenuItem')
 // }
-//
-// // module.exports = require('./DropdownMenu');
+
+module.exports = require('./DropdownMenu');
